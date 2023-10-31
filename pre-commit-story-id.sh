bf3422c035607826f35301a808cab430840f04ee
Manuel Winkler <110164826+SwissCheese15@users.noreply.github.com>
2023-10-31 13:14:57 +0100

    Update pre-commit-story-id.sh

--- pre-commit-story-id.sh
+++ pre-commit-story-id.sh
@@ -6,9 +6,9 @@ project_id=$(echo "$branch_name" | sed -n 's/^.*\/\([^-]*\)-.*$/\1/p')  # Extrac
 if [ -n "$project_id" ]; then
     # If a Project-ID is found in the branch name, modify the commit message
     sed -i "1s/$/ \[$project_id\]/" "$1"
-    alert "Story ID [$project_id] was automatically added to the commit message."
+    echo "Story ID [$project_id] was automatically added to the commit message."
 else
     # If no Project-ID is found, display a message
-    alert "No Story ID found in branch name."
+    echo "No Story ID found in branch name."
     exit 1
 fi

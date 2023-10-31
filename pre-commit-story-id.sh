cb9640251b62caff1c16eee26c170283e552ccc4
Manuel Winkler <110164826+SwissCheese15@users.noreply.github.com>
2023-10-31 10:23:54 +0100

    Create pre-commit-story-id.sh

---
+++ pre-commit-story-id.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+branch_name=$(git symbolic-ref --short HEAD)  # Get the current branch name
+project_id=$(echo "$branch_name" | sed -n 's/^.*\/\([^-]*\)-.*$/\1/p')  # Extract the Project-ID
+
+if [ -n "$project_id" ]; then
+    # If a Project-ID is found in the branch name, modify the commit message
+    sed -i "1s/$/ \[$project_id\]/" "$1"
+    echo "Story ID [$project_id] was automatically added to the commit message."
+else
+    # If no Project-ID is found, display a message
+    echo "No Story ID found in branch name."
+    exit 1
+fi
